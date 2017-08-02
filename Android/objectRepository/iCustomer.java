3657c7dab332f3a935045bfe9881fbb6ce13aa90
JasmeetSomal <singhjasmeet@seasiainfotech.com>
2017-08-02 17:40:19 +0530

    UpdateS
    First Commit

--- Android/objectRepository/iCustomer.java
+++ Android/objectRepository/iCustomer.java
@@ -9,6 +9,12 @@ public interface iCustomer {
 	String id_SignupScrHeader = "co.jakapp.customer:id/toolbar_title";
 	String id_FacebookLoginPageLayout = "com.facebook.katana:id/login_root";
 	String id_GooglePopupTitle = "com.google.android.gms:id/title";
+	String id_ForgotPwdScrEmailTB = "co.jakapp.customer:id/email_et";
+	String id_ForgotPwdScrPhoneTB = "co.jakapp.customer:id/phone_number_et";
+	String id_ForgotPwdScrEmailLbl = "co.jakapp.customer:id/email_radio_btn";
+	String id_ForgotPwdScrSMSLbl = "co.jakapp.customer:id/sms_radio_btn";
+	String id_ForgotPwdScrSendBtn = "co.jakapp.customer:id/reset_btn";
+	String id_ForgotPwdScrValidationMsgs = "co.jakapp.customer:id/snackbar_text";
 	
 	String id_FacebookAppLoginScrUsernameTB = "com.facebook.katana:id/login_username";
 	String id_FacebookAppLoginScrPasswordTB = "com.facebook.katana:id/login_password";
